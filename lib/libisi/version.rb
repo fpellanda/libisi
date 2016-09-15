5261eb69c029ea47d3efcfb655c552af961be329
Flavio Pellanda <flavio.pellanda@localsearch.ch>
2016-09-15 14:42:52 +0200

    Gem init

---
+++ lib/libisi/version.rb
@@ -0,0 +1,3 @@
+module Libisi
+  VERSION = "0.1.0"
+end
---
+++ spec/libisi_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Libisi do
+  it 'has a version number' do
+    expect(Libisi::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
